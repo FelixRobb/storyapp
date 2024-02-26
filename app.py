8fde90b97ccd4cca6666012e7ed47dea7ede6c80
FelixRobb <felix.nunes.robb@gmail.com>
2024-02-26 19:04:12 +0000

    userpage settings

--- app.py
+++ app.py
@@ -304,6 +304,12 @@ class RequestPasswordResetForm(FlaskForm):
     submit = SubmitField('Reset Password')
 
 
+# Custom error handler
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('error.html'), 500
+
+    
 # Routes
 
 @login_manager.user_loader
